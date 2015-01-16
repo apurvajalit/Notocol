f8d97ec3784e1176280b83b4267542a64314fa4a
nileshgarg <nileshgarg2008@gmail.com>
2015-01-16 23:46:32 +0530

    Project added

---
+++ Notocol/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Notocol.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Notocol/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Notocol.WebApiApplication" Language="C#" %>
