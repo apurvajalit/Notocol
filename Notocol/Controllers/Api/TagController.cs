acaa3350a3889238499a3dc516b223be1039d23e
nileshgarg <nileshgarg2008@gmail.com>
2015-03-15 12:38:58 +0530

    Tag API integrated

--- Notocol/Controllers/Api/TagController.cs
+++ Notocol/Controllers/Api/TagController.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
+using Model;
+using Repository;
 
 namespace Notocol.Controllers.Api
 {
     public class TagController : BaseApiController
     {
+        [HttpGet]
+        public IList<Tag> Tags(string strSearch)
+        {
+            TagRepository objTagRepository = new TagRepository();
+            IList<Tag> searchTags = objTagRepository.SearchTags(strSearch, 2);
+            return searchTags;
+        }
+
+        [HttpPost]
+        public long Tag(Tag objTag)
+        {
+            objTag.UserID = 2;
+            TagRepository objTagRepository = new TagRepository();
+            return objTagRepository.SaveTag(objTag).ID;
+        }
+
+        [HttpDelete]
+        public bool DeleteTag(Tag objTag)
+        {
+            objTag.UserID = 2;
+            TagRepository objTagRepository = new TagRepository();
+            return objTagRepository.DeleteTag(objTag);
+        }
+
     }
 }
