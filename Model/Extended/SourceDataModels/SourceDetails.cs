04915c5a6592a1ecc713887f406afdd92e377899
apurvajalit <apurva.jalit@gmail.com>
2015-04-08 13:39:37 +0530

    Adding API for source details for extension

---
+++ Model/Extended/SourceDataModels/SourceDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notocol.Models
+
+{
+    public class SourceDetails
+    {
+        public string url;
+        public long id;
+    }
+}
---
+++ Notocol/Controllers/Api/SourceDataForExtensionController.cs
@@ -0,0 +1,20 @@
+﻿using Notocol.Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notocol.Controllers.Api
+{
+    public class SourceDataForExtensionController:BaseApiController
+    {
+        [HttpGet]
+        public SourceDataForExtension GetSourceData(string pageURL, long userID)
+        {
+            SourceRepository obSourceRepository = new SourceRepository();
+            return obSourceRepository.getSourceData(pageURL, userID);
+        }
+    }
+}
