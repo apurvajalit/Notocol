9b838c30f0ee4ee30750dc1f85a18b751290385d
nileshgarg <nileshgarg2008@gmail.com>
2015-04-21 00:10:31 +0530

    Single sign on implemented

---
+++ Notocol/App_Start/AuthConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Web.WebPages.OAuth;
+
+namespace Notocol
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            OAuthWebSecurity.RegisterFacebookClient(
+              appId: "875706079161883",
+              appSecret: "32aef224208419b3144cf4c5bc3baf90");
+            OAuthWebSecurity.RegisterClient(new GooglePlusClient("984532038251-4eia5k9oelbkcados4lq6k6cg20a7irh.apps.googleusercontent.com", "78ew4q_z4uR9_aC1eKImrTaO"), "Google+", null);
+        }
+    }
+}
--- Notocol/Controllers/UserController.cs
+++ Notocol/Controllers/UserController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Repository;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+
 namespace Notocol.Controllers
 {
     public class UserController : Controller
@@ -65,5 +68,77 @@ namespace Notocol.Controllers
                 return View("Error");
             }
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            return new ExternalLoginResult(provider, Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
+        }
+
+        //
+        // GET: /Account/ExternalLoginCallback
+
+        [AllowAnonymous]
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            // Rewrite request before it gets passed on to the OAuth Web Security classes
+            GooglePlusClient.RewriteRequest();
+
+            AuthenticationResult result = OAuthWebSecurity.VerifyAuthentication(Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
+            if (!result.IsSuccessful)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            Session["Provider"] = result.Provider;
+            Session["ProviderUserId"] = result.ProviderUserId;
+            Session["username"] = result.UserName;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                // If the current user is logged in add the new account
+                OAuthWebSecurity.CreateOrUpdateAccount(result.Provider, result.ProviderUserId, User.Identity.Name);
+                return RedirectToLocal(returnUrl);
+            }
+            else
+            {
+                // User is new, ask for their desired membership name
+                Session["Provider"] = result.Provider;
+                Session["ProviderUserId"] = result.ProviderUserId;
+                Session["username"] = result.UserName;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        internal class ExternalLoginResult : ActionResult
+        {
+            public ExternalLoginResult(string provider, string returnUrl)
+            {
+                Provider = provider;
+                ReturnUrl = returnUrl;
+            }
+
+            public string Provider { get; private set; }
+            public string ReturnUrl { get; private set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                OAuthWebSecurity.RequestAuthentication(Provider, ReturnUrl);
+            }
+        }
+
     }
 }
