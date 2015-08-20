06400cb1c7b1fe85d2d331cdd8ca9b6bf8ef4b8c
apurvajalit <apurva.jalit@gmail.com>
2015-08-20 11:39:32 +0530

    Adding User.cs

---
+++ Model/Extended/Extension/ExtensionUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extended.Extension
+{
+    public class ExtensionUser
+    {
+        public long ID { get; set; }
+        public string Username { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+        public string subscriptions { get; set; }
+        public string pwd { get; set; }
+        public string code { get; set; }
+        public string hashkey { get; set; }
+     }
+}
