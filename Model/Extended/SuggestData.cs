43576cd64d72bb0b172a788afb678550f8093fe5
apurvajalit <apurva.jalit@gmail.com>
2015-12-14 16:17:16 +0530

    profile changes

---
+++ Model/Extended/SuggestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extended
+{
+    public class SuggestData
+    {
+        public string text { get; set; }
+        public int type { get; set; }
+    }
+}
---
+++ Model/Extended/UserProfileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extended
+{
+    public class UserProfileInfo
+    {
+        public string name { get; set; }
+        public string userName { get; set; }
+        public long ID { get; set; }
+        public int followers { get; set; }
+        public int follows { get; set; }
+        public int numberOfNotes { get; set; }
+        public int numberOfPages { get; set; }
+        public int  numberofCollections { get; set; }
+        public List<string> recentTags { get; set; }
+        public List<Folder> collections{ get; set; }
+    }
+}
